614cf56411116d58baa8f8ef21ef59942ee24097
Iskandar Rassulov <iskander.rassulov.2002@gmail.com>
2024-05-02 03:13:30 +0200

    init commit

---
+++ app/src/main/java/com/example/tictactoegame/MainActivity.kt
@@ -0,0 +1,23 @@
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tictactoegame.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        val playButton = findViewById<Button>(R.id.playButton)
+//        playButton.setOnClickListener {
+//            val intent = Intent(this, GameActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        val exitButton = findViewById<Button>(R.id.exitButton)
+//        exitButton.setOnClickListener {
+//            finish()
+//        }
+    }
+}
